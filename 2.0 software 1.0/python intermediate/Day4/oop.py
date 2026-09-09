eaca345a084803fa0675f583f1431ba732d0c904
sirmbom <atsimbomgwe31@outlook.com>
2026-09-09 20:44:41 +0100

    remove unnecessary pass statement in Student class and fix print statement formatting

--- 2.0 software 1.0/python intermediate/Day4/oop.py
+++ 2.0 software 1.0/python intermediate/Day4/oop.py
@@ -7,7 +7,6 @@ class Student:
         self.matricule = matricule
         self.gpa = gpa
         self.gender = gender
-        pass
 
     # Behaviours/functions
     def register(self):
@@ -23,4 +22,4 @@ second_student = Student("Nita", "UBa23PB025", 2.0, "Male")
 
 print(f"First Student:\n{first_student.name}, {first_student.matricule}, {first_student.gpa}, {first_student.gender}")
 
-print(f"First Student {first_student.name} is Taking Lesson {first_student.drop_lesson}")
+print(f"First Student {first_student.name} is Taking Lesson {first_student.drop_lesson()}")
